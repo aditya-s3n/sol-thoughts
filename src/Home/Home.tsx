1581c815786f443c418fd68738243cbf3fdc71ec
Schrodingers-Cats <adityasen120@gmail.com>
2022-04-12 18:50:20 -0400

    Add React .tsx files

---
+++ src/Home/Home.tsx
@@ -0,0 +1,14 @@
+//import react modules
+import React from "react";
+
+//render HOME function
+function Home() {
+    return (
+        <div>
+
+        </div>
+    );
+}
+
+//export
+export default Home;
