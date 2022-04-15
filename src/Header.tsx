7f6ab9049f45ef752548e9418ba827776947dde9
Schrodingers-Cats <adityasen120@gmail.com>
2022-04-15 12:18:59 -0400

    finish Title

--- src/Header.tsx
+++ src/Header.tsx
@@ -4,26 +4,26 @@ import React from "react";
 //render Header 
 function Header() {
     return (
-        <nav class="navbar navbar-expand-lg navbar-light bg-light">
+        <nav className="navbar navbar-expand-lg navbar-light bg-light">
 
-            <div class="container-fluid">
+            <div className="container-fluid">
 
-                <a class="navbar-brand h5" href="/">Mind of the Sun</a>
+                <a className="navbar-brand h5" href="/">Mind of the Sun</a>
 
-                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                    <span class="navbar-toggler-icon"></span>
+                <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                    <span className="navbar-toggler-icon"></span>
                 </button>
 
-                <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <div className="collapse navbar-collapse" id="navbarSupportedContent">
 
-                    <ul class="navbar-nav ms-auto">
+                    <ul className="navbar-nav ms-auto">
 
-                        <li class="nav-item">
-                            <a class="nav-link active h5" aria-current="page" href="/">Home</a>
+                        <li className="nav-item">
+                            <a className="nav-link active h5" aria-current="page" href="/">Home</a>
                         </li>
 
-                        <li class="nav-item nav-shortcut">
-                            <a class="nav-link active h5" aria-current="page" href="/threads">Threads</a>
+                        <li className="nav-item nav-shortcut">
+                            <a className="nav-link active h5" aria-current="page" href="/threads">Threads</a>
                         </li>
 
                     </ul>
--- src/Title.tsx
+++ src/Title.tsx
@@ -5,9 +5,9 @@ import React from "react";
 //props: title on what to render
 function Title(props) {
     return (
-        <div class="container">
-            <h2>{props.title}</h2>
-            <hr style={{"width": "50%"}}/>
+        <div className="container-fluid mt-2 center-text">
+            <h2 className="display-4 my-4">{props.title}</h2>
+            <hr style={{"width": "50%"}} className="mx-auto"/>
         </div>
     );
 }
